77576bd90e2216ea45965ba41d902a6fda336b8a
scalcantara <alcantara.s@icloud.com>
2022-08-23 15:28:48 -0400

    sec com

---
+++ common/styles-fonts.php
@@ -0,0 +1,11 @@
+
+<meta charset="utf-8">
+<link href="./styles/style.css" rel="stylesheet" type="text/css">
+<link href="./styles/products-style.css" rel="stylesheet" type="text/css">
+<link href="./styles/contact-style.css" rel="stylesheet" type="text/css">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<script src="script.js" defer></script>
